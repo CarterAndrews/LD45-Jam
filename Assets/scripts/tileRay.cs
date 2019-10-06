4e5850a4226944a15925bb4d0dfd4ffb1f9a0f99
Carter Andrews <andrewscart@gmail.com>
2019-10-05 19:30:27 -0700

    reorganization

--- Assets/scripts/tileRay.cs
+++ Assets/scripts/tileRay.cs
@@ -6,32 +6,40 @@ public class tileRay : MonoBehaviour
 {
     public Tilemap tiles;
     public TileBase placingBuilding;
-    public TileBase tent;
-    public TileBase suite;
     public SpriteRenderer mouseIcon;
     public TileBase airTile;
+    public tileType[] tiletypes;
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
 
-    public void changePlacingTile(TileBase newTileType)
+    }
+    [System.Serializable]
+    public struct tileType{
+        public TileBase tile;
+        public Sprite spr;
+    }
+    public void changePlacingTile(tileType t)
     {
-        placingBuilding = newTileType;
+        placingBuilding = t.tile;
+        mouseIcon.sprite = t.spr;
     }
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.W))
         {
-            changePlacingTile(tent);
+            changePlacingTile(tiletypes[1]);
         }
         if (Input.GetKeyDown(KeyCode.E))
         {
-            changePlacingTile(suite);
+            changePlacingTile(tiletypes[0]);
         }
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButton(1))
+        {
+            placingBuilding = null;
+        }
+        if (Input.GetMouseButtonDown(0)&&placingBuilding!=null)
         {
             Vector3 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition + new Vector3(0, 0, 10));
             var tile = tiles.GetTile(tiles.WorldToCell(pos));
@@ -39,7 +47,6 @@ public class tileRay : MonoBehaviour
             {
                 tiles.SetTile(tiles.WorldToCell(pos), placingBuilding);
             }
-            print(tile);
         }
     }
     
